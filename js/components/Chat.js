197223cbbe4daaacf4f0f9c2d9643b3d2a4a4eb5
drugpamir <drugpamir@users.noreply.github.com>
2025-02-14 18:45:28 +0000

    deploy: 3a39a26a184c0c6303f9bce8c77f9c1f58fb1cbb

---
+++ js/components/Chat.js
@@ -0,0 +1,13 @@
+const URL_PREFIX = "/chat";
+
+const Chat = () => {
+  return `
+    <section class="chat-section">
+        <div class="chat-title">Чат</div>
+        <div class="chat-messages">Сообщения</div>
+     </section>
+     `;
+};
+
+export default Chat;
+export { URL_PREFIX };
