f164a91ac9c40664551f3df53b576098ee1a903b
drugpamir <toll_aa@mail.ru>
2025-02-14 21:24:49 +0300

    added ArticleList component

--- src/js/components/Navbar.js
+++ src/js/components/Navbar.js
@@ -1,8 +1,10 @@
+import { URL_PREFIX } from "./ArticleList";
+
 const NavBar = () => `
     <header>
       <nav class="navbar">
         <ul class="nav-list">
-          <li><a href="arcticles" class="nav-link">Статьи</a></li>
+          <li><a href="${URL_PREFIX}" class="nav-link">Статьи</a></li>
           <li><a href="chat" class="nav-link">Онлайн-чат</a></li>
         </ul>
       </nav>
---
+++ src/js/data.js
@@ -0,0 +1,4 @@
+export const articles = [
+  { id: 1, title: "JS basic" },
+  { id: 2, title: "JS professional" },
+];
