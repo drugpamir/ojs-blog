29073f7608eb594b108c0e347cb2037c347e251e
drugpamir <drugpamir@users.noreply.github.com>
2025-02-14 17:11:50 +0000

    deploy: 09b0a8b1b906309061dfa22f7ede86bc95ce7277

---
+++ js/navbar.js
@@ -0,0 +1,12 @@
+const NavBar = () => `
+    <header>
+      <nav class="navbar">
+        <ul class="nav-list">
+          <li><a href="arcticles" class="nav-link">Статьи</a></li>
+          <li><a href="chat" class="nav-link">Онлайн-чат</a></li>
+        </ul>
+      </nav>
+    </header>   
+`;
+
+export default NavBar;
