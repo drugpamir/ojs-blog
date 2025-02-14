42871fc35722ba76220e525fbd208a4eed33ac05
drugpamir <toll_aa@mail.ru>
2025-02-14 19:35:51 +0300

    added webpack, linters, hooks

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import jest from "eslint-plugin-jest";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        module: "writable",
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  {
+    ...jest.configs["flat/recommended"],
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+  },
+  {
+    ignores: ["dist", "webpack.config*"],
+  },
+  eslintConfigPrettier,
+];
